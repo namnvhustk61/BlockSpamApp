5021790d401dad860f0f2ffa648858af16aded19
NamReal <namrealbk10@gmail.com>
2021-03-06 12:41:46 +0700

    ..

--- app/src/main/java/com/stork/blockspam/MainActivity.kt
+++ app/src/main/java/com/stork/blockspam/MainActivity.kt
@@ -10,6 +10,7 @@ import android.telecom.TelecomManager
 import androidx.annotation.RequiresApi
 import com.stork.blockspam.base.BaseActivity
 import com.stork.blockspam.utils.AppPermission
+import com.stork.blockspam.utils.AppSettingsManager
 import java.lang.Exception
 
 
@@ -25,7 +26,8 @@ class MainActivity : BaseActivity() {
 
     private fun setPermission(){
         //set APP is caller id & spam app default
-        requestRole()
+        AppSettingsManager.setDefaultAppBlockCall(this)
+
         // Danh ba ---- READ_PHONE_STATE
         AppPermission.requirePermissions(this, AppPermission.PER_READ_PHONE_STATE, AppPermission.PER_REQUEST_CODE)
         AppPermission.requirePermissions(this, AppPermission.PER_READ_CONTACTS, AppPermission.PER_REQUEST_CODE)
@@ -47,39 +49,9 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    /*
-    *   set APP is caller id & spam app default
-    *  dialog permission caller id & spam app
-    *
-    * */
-
-    private val ROLE_CALL_SCREENING_ID = 100
-
-    private fun requestRole() {
-        try {
-            val roleManager = getSystemService(Context.ROLE_SERVICE)
-            if(roleManager  is RoleManager){
-                val intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING)
-                startActivityForResult(intent, ROLE_CALL_SCREENING_ID)
-            }
-        }catch (e: Exception){}
-    }
-
-    private fun setDefaultCallPhone(){
-        val intent = Intent(TelecomManager.ACTION_CHANGE_DEFAULT_DIALER)
-        intent.putExtra(TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME, packageName)
-        startActivity(intent)
-    }
-    
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun gotoSettings(){
-        var i = Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS)
-        startActivity(i)
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == ROLE_CALL_SCREENING_ID) {
+        if (requestCode == AppSettingsManager.ROLE_CALL_SCREENING_ID) {
             if (resultCode == android.app.Activity.RESULT_OK) {
                 // Your app is now the call screening app
             } else {
---
+++ app/src/main/java/com/stork/blockspam/utils/AppSettingsManager.kt
@@ -0,0 +1,38 @@
+package com.stork.blockspam.utils
+
+import android.app.Activity
+import android.app.role.RoleManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import android.telecom.TelecomManager
+import androidx.annotation.RequiresApi
+import java.lang.Exception
+
+object AppSettingsManager {
+
+
+    val ROLE_CALL_SCREENING_ID = 100
+    fun setDefaultAppBlockCall(activity: Activity?){
+        try {
+            val roleManager = activity?.getSystemService(Context.ROLE_SERVICE)
+            if(roleManager  is RoleManager){
+                val intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING)
+                activity.startActivityForResult(intent, ROLE_CALL_SCREENING_ID)
+            }
+        }catch (e: Exception){}
+    }
+
+    private fun setDefaultAppCallPhone(activity: Activity?){
+        val intent = Intent(TelecomManager.ACTION_CHANGE_DEFAULT_DIALER)
+        intent.putExtra(TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME, activity?.packageName)
+        activity?.startActivity(intent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun gotoSettingsDefaultApp(activity: Activity?){
+        val intent = Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS)
+        activity?.startActivity(intent)
+    }
+}
